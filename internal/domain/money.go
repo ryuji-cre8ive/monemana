9f3222eec99a7f700d9b7dea8f0d0a10d5b9e587
ryuji-cre8ive <ryujisoccer0327@gmail.com>
2023-09-25 14:24:04 +0800

    domain作成

--- internal/domain/money.go
+++ internal/domain/money.go
@@ -1,1 +1,12 @@
 package domain
+
+import (
+	"time"
+)
+
+type Money struct {
+	ID     string
+	Name   string
+	Amount int
+	Date   time.Time
+}
---
+++ internal/domain/user.go
@@ -0,0 +1,10 @@
+package domain
+
+import ()
+
+type User struct {
+	ID        string
+	FirstName string
+	LastName  string
+	Money     []Money
+}
