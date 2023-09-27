a9498bef5834bc7c13db0dea3d3198dcfc68b385
ryuji-cre8ive <ryujisoccer0327@gmail.com>
2023-09-27 14:18:53 +0800

    ui層作成

---
+++ internal/ui/main.go
@@ -0,0 +1,35 @@
+package ui
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/ryuji-cre8ive/monemana/internal/usecase"
+)
+
+type Handler struct {
+	WebhookHandler
+}
+
+func New(u *usecase.Usecase) *Handler {
+	return &Handler{
+		WebhookHandler: &webhookHandler{},
+	}
+}
+
+func SetApi(e *echo.Echo, h *Handler) {
+	g := e.Group("/api/v1")
+	g.POST("/webhook", h.WebhookHandler.PostWebhook)
+}
+
+func Echo() *echo.Echo {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+	}))
+
+	return e
+}
---
+++ internal/ui/webhook.go
@@ -0,0 +1,26 @@
+package ui
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/ryuji-cre8ive/monemana/internal/usecase"
+
+	"golang.org/x/xerrors"
+)
+
+type (
+	WebhookHandler interface {
+		PostWebhook(c echo.Context) error
+	}
+
+	webhookHandler struct {
+		usecase.WebhookUsecase
+	}
+)
+
+func (h *webhookHandler) PostWebhook(c echo.Context) error {
+	err := h.WebhookUsecase.PostWebhook(c)
+	if err != nil {
+		return xerrors.Errorf("failed to post webhook: %w", err)
+	}
+	return nil
+}
