a4a1ca8112ba27eb6dfffe1539ed6b7beb98fa66
ryuji-cre8ive <ryujisoccer0327@gmail.com>
2024-06-26 17:38:25 +0900

    DBの構成を見直しました

--- internal/database/migrations/20230927120532-create_transaction.sql
+++ internal/database/migrations/20230927120532-create_transaction.sql
@@ -2,19 +2,45 @@
 -- +migrate Up
 CREATE TABLE "users" (
     id text PRIMARY KEY,
-    name text
+    display_name text,
+    created_at timestamp,
+    updated_at timestamp,
+    deleted_at timestamp
 );
 
-CREATE TABLE Transactions (
+CREATE TABLE rooms (
+    id text PRIMARY KEY,
+    display_name text,
+    created_at timestamp,
+    updated_at timestamp,
+    deleted_at timestamp
+);
+
+CREATE TABLE rooms_binding_users (
+    id text PRIMARY KEY,
+    room_id text,
+    user_id text,
+    CONSTRAINT FK_room_binding_users_users FOREIGN KEY (user_id) REFERENCES "users" (id) ON DELETE CASCADE,
+    CONSTRAINT FK_room_binding_users_rooms FOREIGN KEY (room_id) REFERENCES "rooms" (id) ON DELETE CASCADE
+);
+
+CREATE TABLE transactions (
     id text PRIMARY KEY,
     title text,
     price decimal,
     user_id text,
-    date timestamp,
-    rate decimal,
-    CONSTRAINT FK_transactions_users FOREIGN KEY (user_id) REFERENCES "users" (id) ON DELETE CASCADE
+    target_user_id text,
+    room_id text,
+    created_at timestamp,
+    deleted_at timestamp,
+    CONSTRAINT FK_transactions_users FOREIGN KEY (user_id) REFERENCES "users" (id) ON DELETE CASCADE,
+    CONSTRAINT FK_transactions_target_users FOREIGN KEY (target_user_id) REFERENCES "users" (id) ON DELETE CASCADE,
+    CONSTRAINT FK_transactions_rooms FOREIGN KEY (room_id) REFERENCES "rooms" (id) ON DELETE CASCADE
 );
 
 -- +migrate Down
-DROP TABLE Transactions;
+DROP TABLE transactions;
+DROP TABLE rooms_binding_users;
+DROP TABLE rooms;
 DROP TABLE "users";
+
