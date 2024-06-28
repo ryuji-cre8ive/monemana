1b854195f52e18f9c4025593bb00eb16b44bc94a
ryuji-cre8ive <ryujisoccer0327@gmail.com>
2024-06-28 11:56:41 +0900

    feat[new]: validate request headers to improve security

--- internal/ui/main.go
+++ internal/ui/main.go
@@ -1,11 +1,13 @@
 package ui
 
 import (
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/ryuji-cre8ive/monemana/internal/usecase"
+	"github.com/ryuji-cre8ive/monemana/internal/utils"
 )
 
 type Handler struct {
@@ -25,6 +27,13 @@ func SetApi(e *echo.Echo, h *Handler) {
 			signature := c.Request().Header.Get("X-Line-Signature")
 			userAgent := c.Request().Header.Get("User-Agent")
 			if signature != "" && userAgent == "LineBotWebhook/2.0" {
+				bodyBytes, err := io.ReadAll(c.Request().Body)
+				if err != nil {
+					return c.String(http.StatusInternalServerError, "Failed to read request body")
+				}
+				if !utils.IsValidSignature(signature, bodyBytes) {
+					return c.String(http.StatusUnauthorized, "Invalid signature")
+				}
 				return next(c)
 			} else {
 				return c.String(http.StatusUnauthorized, "Invalid signature or User-Agent")
