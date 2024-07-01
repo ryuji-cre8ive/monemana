ab0c2de55e6aa6976bcb3710ca5f02192a2cdb47
kokubu-ryuji <kokubu-ryuji@dmm.com>
2024-07-01 12:46:51 +0900

    feat[fix]: delete validation

--- internal/ui/main.go
+++ internal/ui/main.go
@@ -1,13 +1,9 @@
 package ui
 
 import (
-	"io"
-	"net/http"
-
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/ryuji-cre8ive/monemana/internal/usecase"
-	"github.com/ryuji-cre8ive/monemana/internal/utils"
 )
 
 type Handler struct {
@@ -22,28 +18,8 @@ func New(u *usecase.Usecase) *Handler {
 
 func SetApi(e *echo.Echo, h *Handler) {
 	g := e.Group("/api/v1")
-	authMiddleware := func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			signature := c.Request().Header.Get("X-Line-Signature")
-			userAgent := c.Request().Header.Get("User-Agent")
-			if signature != "" && userAgent == "LineBotWebhook/2.0" {
-				bodyBytes, err := io.ReadAll(c.Request().Body)
-				if err != nil {
-					return c.String(http.StatusInternalServerError, "Failed to read request body")
-				}
-				if !utils.IsValidSignature(signature, bodyBytes) {
-					return c.String(http.StatusUnauthorized, "Invalid signature")
-				}
-				return next(c)
-			} else {
-				return c.String(http.StatusUnauthorized, "Invalid signature or User-Agent")
-			}
-		}
-	}
-
-	g.POST("/webhook", h.WebhookHandler.PostWebhook, authMiddleware)
+	g.POST("/webhook", h.WebhookHandler.PostWebhook)
 	g.GET("/healthcheck", HealthCheckHandler)
-
 }
 
 func Echo() *echo.Echo {
